6856fb702efe479616b9a170924bc713bcc5bab6
frazz11 <francesco.zappa1@gmail.com>
2024-03-21 17:55:16 +0100

    Fix linting

--- Derivatives.py
+++ Derivatives.py
@@ -1,5 +1,6 @@
 import numpy as np
-from Stencils import stencils, Stencils
+from Stencils import Stencils
+
 
 class Derivatives:
     """
@@ -10,26 +11,26 @@ class Derivatives:
                      grid_array,
                      num_ghost,
                      array):
-            self.actual_size = np.size(grid_array) - 2*num_ghost           
+            self.actual_size = np.size(grid_array) - 2*num_ghost
             self.dx = grid_array[1]-grid_array[0]
             self.oodx = 1./self.dx
             self.is_ = num_ghost
             self.ie_ = num_ghost+self.actual_size
             self.derivative = np.zeros_like(grid_array)
 
-
         def calculate_derivative(self,
-                         array,
-                         order,
-                         accuracy_order):
+                                 array,
+                                 order,
+                                 accuracy_order):
             N = self.actual_size+2*self.is_
 
             stencil = Stencils(order, accuracy_order)
 
-
             S_2 = stencil.width//2
             for i, s in enumerate(stencil.stencil):
-                self.derivative[self.is_:self.ie_] = self.derivative[self.is_:self.ie_] + s * array[self.is_ + i-S_2 : self.ie_ + i-S_2]
+                self.derivative[self.is_:self.ie_] = \
+                        self.derivative[self.is_:self.ie_]\
+                        + s * array[self.is_ + i-S_2:self.ie_ + i-S_2]
             self.derivative = self.derivative * self.oodx**order
             self.derivative[0:self.is_] = np.nan
             self.derivative[self.ie_:N] = np.nan
@@ -40,22 +41,21 @@ class Derivatives:
             return self.is_, self.ie_
 
     def __init__(self,
-                 order = 1,
-                 accuracy_order = 2):
+                 order=1,
+                 accuracy_order=2):
         self.order = order
         self.accuracy_order = accuracy_order
         self.num_ghost = accuracy_order//2
 
-
-
     def __str__(self):
-        return f"Derivative of order {self.order} and order of accuracy {self.accuracy_order}."
-    
+        return f"Derivative of order {self.order}\
+                and order of accuracy {self.accuracy_order}."
+
     def calculate_derivative(self,
-                            grid_array,
-                            array):
+                             grid_array,
+                             array):
         derivative = self.Derivative(grid_array, self.num_ghost, array)
-        derivative.calculate_derivative(array,self.order,self.accuracy_order)
+        derivative.calculate_derivative(array, self.order, self.accuracy_order)
 
         return derivative
 
@@ -63,4 +63,3 @@ class Derivatives:
 if __name__ == "__main__":
     ex = Derivatives()
     print(ex)
-
